69700d6e319870ec4c217280fcb570ae751c6bfe
anstnsp <anstnsp@naver.com>
2021-05-15 00:09:45 +0900

    ef

--- src/main/kotlin/com/example/kotlindemo/TestController.kt
+++ src/main/kotlin/com/example/kotlindemo/TestController.kt
@@ -21,6 +21,10 @@ class TestController {
         site.title = "바뀜"
         println(site.title)
         println("wfwef")
+        println("wfwef")
+        println("wfwef")
+
+        println("wfwef")
         return site;
     }
 }
